494ccda712dffbfb83c52cc711fd9921ce8eca12
DolphinWing <dolphinwing74+github@gmail.com>
2026-07-21 00:26:24 +0800

    prepare for linux

--- oni-mods/FontLoader/FontUtil.cs
+++ oni-mods/FontLoader/FontUtil.cs
@@ -8,11 +8,36 @@ namespace FontLoader.Utils
 {
     public static class FontUtil
     {
+        private static string GetPlatformFolder()
+        {
+            // 包含 Editor 環境的判斷，確保在開發階段的 Behavior 一致
+            return Application.platform switch
+            {
+                RuntimePlatform.WindowsPlayer => "win",
+                RuntimePlatform.WindowsEditor => "win",
+
+                RuntimePlatform.OSXPlayer => "mac",
+                RuntimePlatform.OSXEditor => "mac",
+
+                RuntimePlatform.LinuxPlayer => "linux",
+                RuntimePlatform.LinuxEditor => "linux",
+                _ => "other"
+            };
+        }
+
         public static TMP_FontAsset LoadFontAsset(FontConfig config)
         {
             try
             {
-                var platform = Application.platform == RuntimePlatform.WindowsPlayer ? "win": "other";
+                var platform = GetPlatformFolder();
+                // Application.platform == RuntimePlatform.WindowsPlayer ? "win": "other";
+                if (platform == null || platform == "other")
+                {
+                    // 明確告知是因為平台不支援而返回 null，而不是檔案找不到
+                    Debug.LogError($"[FontLoader] Aborting load. Platform {Application.platform} is intentionally not supported.");
+                    return null;
+                }
+
                 var fileName = (config.Filename == "openhuninn") ? "openhuninn.u6" : config.Filename;
                 var assetPath = Path.Combine(ConfigManager.Instance.configPath, "Assets", platform, fileName);
                 Debug.Log($"[FontLoader] {platform} {assetPath}");
